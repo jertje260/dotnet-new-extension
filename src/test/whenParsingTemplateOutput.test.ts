5cd9590d4ffc629ed6d22f7a792cee9d7d828ee8
Jeroen Broekhuizen <jeroen.broekhuizen@jedlix.com>
2019-05-08 21:53:27 +0200

    fix tests

--- src/test/whenParsingTemplateOutput.test.ts
+++ src/test/whenParsingTemplateOutput.test.ts
@@ -7,24 +7,30 @@ import * as testTemplates from './testTemplates';
 let template: Template;
 // Defines a Mocha test suite to group tests of similar kind together
 suite("when parsing template output", function () {
-    beforeEach(()=>{
+    beforeEach(() => {
         template = new Template("testTemplate", "test", [], "", []);
     })
 
-    test("given console template should have author 'Microsoft'", function() {
+    test("given console template should have author 'Microsoft'", function () {
         handleTemplate(testTemplates.consoleTemplate)
 
-        assert("Microsoft", template.author);
+        assert.equal("Microsoft", template.author);
     });
 
-    test("given console template should have description 'A project for creating a command-line application that can run on .NET Core on Windows, Linux and macOS'", function() {
+    test("given console template should have description 'A project for creating a command-line application that can run on .NET Core on Windows, Linux and macOS'", function () {
         handleTemplate(testTemplates.consoleTemplate)
 
-        assert("A project for creating a command-line application that can run on .NET Core on Windows, Linux and macOS", 
+        assert.equal("A project for creating a command-line application that can run on .NET Core on Windows, Linux and macOS",
             template.description);
+    });
+
+    test("given console template should have two options", function () {
+        handleTemplate(testTemplates.consoleTemplate)
+
+        assert.equal(2, template.options.length);
     });
 });
 
-function handleTemplate(input: string){
+function handleTemplate(input: string) {
     handler.handleDotnetTemplateOutput(template, input);
 }
